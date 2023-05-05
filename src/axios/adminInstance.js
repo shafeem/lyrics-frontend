61e8256454994e460c0732af39162552e598f10a
shafeem badusha <shafeemshan5303@gmail.com>
2023-05-05 20:19:28 +0530

    axios

--- src/axios/adminInstance.js
+++ src/axios/adminInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'http://localhost:5000/admin'
+const baseUrl = 'https://lyrics-backend.onrender.com/admin'
 
 const instance = axios.create({
     baseURL:baseUrl,
--- src/axios/userInstance.js
+++ src/axios/userInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:5000/'
+const baseUrl = 'https://lyrics-backend.onrender.com/'
 
 const instance = axios.create({
     baseURL:baseUrl,
